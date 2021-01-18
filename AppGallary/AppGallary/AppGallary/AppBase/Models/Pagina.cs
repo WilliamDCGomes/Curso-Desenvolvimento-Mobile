73b79c3cdf9befeb257bd1e8d001a4a49ea5d5b2
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-01-18 14:20:58 -0300

    Menu - Prog - MenuLateral - Classes Pagina e PaginaColecao

---
+++ AppGallary/AppGallary/AppGallary/AppBase/Models/Pagina.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppGallary.AppBase.Models
+{
+    public class Pagina
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public Type ArquivoPagina { get; set; }
+        public bool IsNavegacao { get; set; }
+    }
+}
