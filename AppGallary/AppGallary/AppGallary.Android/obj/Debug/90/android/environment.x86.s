698e0ab82426cf61da52e25261ab39fb8fca7f7b
WilliamDCGomes <williamdouglasgomes@hotmail.com>
2021-01-20 09:33:08 -0300

    SubtitleComponentView - Criando Componente

--- AppGallary/AppGallary/AppGallary.Android/obj/Debug/90/android/environment.x86.s
+++ AppGallary/AppGallary/AppGallary.Android/obj/Debug/90/android/environment.x86.s
@@ -71,7 +71,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"3fc6200d-4acc-40a8-a9ab-7b6d29411e68"
+	.asciz	"be4a182f-3f43-4101-b336-df92ca1f60f2"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
